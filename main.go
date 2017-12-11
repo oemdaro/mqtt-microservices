10f216de742d9e7d2f5d9db45d4c9544c6db096a
Daro Oem <oemdaro@gmail.com>
2017-12-11 11:49:00 +0700

    add `Hello World` app for easy to clone to correct GOPATH

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Printf("%s", err)
+	}
+}
